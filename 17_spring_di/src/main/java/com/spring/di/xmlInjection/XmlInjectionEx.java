3304efd711d0f7841b37773f69d3816f3ea037ba
19-web-ksm <19-web-ksm@DESKTOP-F11GACD>
2023-12-04 21:41:49 +0900

    spring 시작

---
+++ 17_spring_di/src/main/java/com/spring/di/xmlInjection/XmlInjectionEx.java
@@ -0,0 +1,30 @@
+package com.spring.di.xmlInjection;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+
+/*
+ * 
+ *  XML Injection
+ * 
+ *  - xml 파일에서 생성된 객체를 다른 클래스에 주입시킨다.
+ * 
+ * */
+
+@Controller
+public class XmlInjectionEx {
+
+	/*
+		// servlet-context.xml 파일의 객체를 주입
+		@Inject // @Autowired
+		private BcryptPasswordEncoder bcryptPasswordEncoder;
+	*/
+	
+	/*
+		// root-context.xml 파일의 객체를 주입
+		@Inject //@Autowired
+		private SqlSession sqlSession;
+	*/
+	
+}
